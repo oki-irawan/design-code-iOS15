5611b82a62cc7b1d5138440cb9f92657a227f2e2
Oki Irawan <oki.irawan@bankraya.co.id>
2022-11-20 17:05:32 +0700

    Initial Commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Oki Irawan on 20/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
